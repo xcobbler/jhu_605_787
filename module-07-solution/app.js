ca6d9aff1ae55d36479babf95620ef2f85ddf2cf
Xavier <xaviercoble@gmail.com>
2024-10-12 16:13:34 -0400

    adds bonus implementation.

--- module-07-solution/app.js
+++ module-07-solution/app.js
@@ -4,27 +4,30 @@
     angular.module('ShoppingListCheckOff', [])
     .controller('ToBuyController', ToBuyController)
     .controller('AlreadyBoughtController', AlreadyBoughtController)
-    .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+    .service('ShoppingListCheckOffService', ShoppingListCheckOffService)
+    .filter('ngcurrency', AngularCurrencyFilter);
     
     ToBuyController.$inject = ['ShoppingListCheckOffService'];
     function ToBuyController(ShoppingListCheckOffService) {
         this.items = ShoppingListCheckOffService.getToBuyItems();
 
-        this.buyItem = ShoppingListCheckOffService.buyItem
+        this.buyItem = ShoppingListCheckOffService.buyItem;
 
     }
 
     AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
     function AlreadyBoughtController(ShoppingListCheckOffService) {
         this.items = ShoppingListCheckOffService.getBoughtItems();
+
+        this.getTotalPrice = ShoppingListCheckOffService.getTotalPrice;
     }
 
     function ShoppingListCheckOffService() {
         var service = this;
       
         var toBuyItems = [
-            {name: "Cookies", quantity: 10},
-            {name: "Pepsi", quantity: 2},
+            {name: "Cookies", quantity: 10, pricePerItem: 5},
+            {name: "Pepsi", quantity: 2, pricePerItem: 2},
         ];
 
         var boughtItems = [];
@@ -33,6 +36,10 @@
             //push the item in the [] returned - not the array of 1 item
             boughtItems.push(toBuyItems.splice(itemIndex, 1)[0]);
         };
+
+        service.getTotalPrice = function(item){
+            return item.quantity * item.pricePerItem
+        }
       
         service.getToBuyItems = function () {
           return toBuyItems;
@@ -41,6 +48,12 @@
         service.getBoughtItems = function () {
             return boughtItems;
           };
+      }
+
+      function AngularCurrencyFilter(){
+        return function(input){
+            return "$$$" + (Math.round(input * 100) / 100).toFixed(2)
+        }
       }
     
     })();
